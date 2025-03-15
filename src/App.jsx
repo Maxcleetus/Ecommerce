99dccd0c32ed7e8e8ffc4f7b609425b1c356adf0
MAX <maxcleetus@gmail.com>
2025-03-15 22:28:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Collection from './pages/Collection'
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+function App(){
+  return(
+    <div>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/collection" element={<Collection/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/product/:productId" element={<Product/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/login" element={<Login/>} /> 
+          <Route path="/placeorder" element={<PlaceOrder/>} />
+          <Route path="/orders" element={<Orders/>} />
+        </Routes>
+    </div> 
+  )
+}
+
+export default App
